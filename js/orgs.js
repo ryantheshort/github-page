5dece65e2bc860c44b27778d5b4328c09eb57e3d
Ryan Short <shorttryan@gmail.com>
2022-09-20 20:19:22 -0400

    adds finishing basic html formatting

---
+++ js/orgs.js
@@ -0,0 +1,16 @@
+const orgs = [
+    {
+      "login": "ccs-summer-2022",
+      "id": 112399320,
+      "node_id": "O_kgDOBrMT2A",
+      "url": "https://api.github.com/orgs/ccs-summer-2022",
+      "repos_url": "https://api.github.com/orgs/ccs-summer-2022/repos",
+      "events_url": "https://api.github.com/orgs/ccs-summer-2022/events",
+      "hooks_url": "https://api.github.com/orgs/ccs-summer-2022/hooks",
+      "issues_url": "https://api.github.com/orgs/ccs-summer-2022/issues",
+      "members_url": "https://api.github.com/orgs/ccs-summer-2022/members{/member}",
+      "public_members_url": "https://api.github.com/orgs/ccs-summer-2022/public_members{/member}",
+      "avatar_url": "https://avatars.githubusercontent.com/u/112399320?v=4",
+      "description": null
+    }
+  ]
